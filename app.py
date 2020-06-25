f19b7dc35e5652872e5a7b5b60fbcb27ba01b9b3
Isaias Velasquez <dragoonbr@gmail.com>
2020-06-25 16:39:04 -0300

    static files added

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, request
 
 
-app = Flask(__name__)
+app = Flask(__name__,
+    static_url_path='',
+    static_folder='web/static',
+    template_folder='web/template')
 
 
 
 @app.route('/')
 def index():
-    return 'Welcome to @vlaskzBot server. Proudly hosted in Heroku.'
+    return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
