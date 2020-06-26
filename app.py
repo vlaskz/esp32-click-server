3e836ab8db529379dafde5277882627617d9978d
Sam Velasquez <atrox.lapa@hotmail.com>
2020-06-26 11:29:40 -0300

    added append function

--- app.py
+++ app.py
@@ -5,14 +5,22 @@ app = Flask(__name__,
     static_url_path='',
     static_folder='web/static',
     template_folder='web/template')
-    
+
+
+def append(name, message):
+    data_to_append = {name: message}
+    with open('messages.json', 'r+') as file:
+        data = json.load(file)
+        data.update(data_to_append)
+        file.seek(0)
+        json.dump(data, file)
 
 @app.route('/', methods=['GET', 'POST'])
 def clicked():
     if request.method == 'POST':
             name = request.values.get('name')
             message = request.values.get('message')
-            print('name:', name, ' message:', message)
+            def append(name, message)
             return app.send_static_file('index.html')
     else:
         return app.send_static_file('index.html')
