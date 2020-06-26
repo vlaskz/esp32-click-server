cb43d2cc7c959897bcde2a7f5d1731dda40fcd82
Sam Velasquez <atrox.lapa@hotmail.com>
2020-06-26 10:33:33 -0300

    corrected clicked() function

--- app.py
+++ app.py
@@ -9,13 +9,12 @@ app = Flask(__name__,
 
 @app.route('/', methods=['GET', 'POST'])
 def clicked():
-if request.method == 'POST':
-        name = request.values.get('name')
-        message = request.values.get('message')
-        print('name:', name, ' message:', message)
-if method =='GET':
-else:
-    return app.send_static_file('index.html')
+    if request.method == 'POST':
+            name = request.values.get('name')
+            message = request.values.get('message')
+            print('name:', name, ' message:', message)
+    else:
+        return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(threaded=True)
